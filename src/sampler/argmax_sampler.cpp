a7a8038a1fb5f23b5900f0d859b0ca5511da1119
tang-jiapeng <3081129260@qq.com>
2026-02-01 20:34:59 +0800

    argmaxSampler cpu实现

--- src/sampler/argmax_sampler.cpp
+++ src/sampler/argmax_sampler.cpp
@@ -3,6 +3,10 @@
 
 namespace sampler {
 size_t ArgmaxSampler::sample(const float* logits, size_t size, void* stream) {
-    return size_t(0);
+    if (device_type_ == base::DeviceType::kDeviceCPU) {
+        size_t next = std::distance(logits, std::max_element(logits, logits + size));
+        return next;
+    }
+    return -1;
 }
 }  // namespace sampler
